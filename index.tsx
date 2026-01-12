ced8ed9928d2257738e833eb1942dc3c29545502
harleyfan91 <harleyfan91@aol.com>
2026-01-12 10:11:39 +0900

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,3 +1,9 @@
+// This MUST be the first thing in the file
+(window as any).process = {
+  env: {
+    API_KEY: import.meta.env.VITE_GEMINI_API_KEY
+  }
+};
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
